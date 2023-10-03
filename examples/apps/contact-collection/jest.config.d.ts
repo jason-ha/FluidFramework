472d26ffb064c61fa45028493930dfd69513bf3e
Jason Hartman <jasonha@microsoft.com>
2023-10-03 11:53:06 -0700

    contact-container to ESM
    webpack does not succeed. It fails to resolve @fluidframework/server-services-client for tinylicious:
    ```log
    ERROR in ../../../packages/drivers/tinylicious-driver/lib/insecureTinyliciousTokenProvider.js 6:0-71
    Module not found: Error: Can't resolve '@fluidframework/server-services-client' in '.../packages/drivers/tinylicious-driver/lib'
    ```

---
+++ examples/apps/contact-collection/jest.config.d.ts
@@ -0,0 +1,24 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+export declare const globals: {
+	PATH: string;
+};
+// Minimal type export above for test script import. Below is the others for full type definition.
+// export declare const preset = "jest-puppeteer";
+// export declare const testMatch: string[];
+// export declare const testPathIgnorePatterns: string[];
+// export declare const transform: {
+// 	"^.+\\.ts?$": string;
+// };
+// export declare const reporters: (
+// 	| string
+// 	| (
+// 			| string
+// 			| {
+// 					outputDirectory: string;
+// 					outputName: string;
+// 			  }
+// 	  )[]
+// )[];
