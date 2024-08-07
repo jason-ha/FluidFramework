5239bb22ce12a118f33f0e940bd61f098a31b585
Jason Hartman <jasonha@microsoft.com>
2024-08-07 16:36:39 -0700

    feat(client): path-based address routing (proposed) For Signals, use a `/` separated address in `IEnvelope`. No change to Ops. Reading is always supported. Writing (sending) requires "2.32" compatibility mode to be enabled.

--- packages/runtime/container-runtime/src/test/containerRuntime.spec.ts
+++ packages/runtime/container-runtime/src/test/containerRuntime.spec.ts
@@ -1477,7 +1477,7 @@ describe("Runtime", () => {
 				mockLogger = new MockLogger();
 			});
 
-			const runtimeOptions: IContainerRuntimeOptionsInternal = {
+			const runtimeOptions = {
 				compressionOptions: {
 					minimumBatchSizeInBytes: 1024 * 1024,
 					compressionAlgorithm: CompressionAlgorithms.lz4,
@@ -1485,9 +1485,9 @@ describe("Runtime", () => {
 				chunkSizeInBytes: 800 * 1024,
 				flushMode: FlushModeExperimental.Async as unknown as FlushMode,
 				enableGroupedBatching: true,
-			};
+			} as const satisfies IContainerRuntimeOptionsInternal;
 
-			const defaultRuntimeOptions: IContainerRuntimeOptionsInternal = {
+			const defaultRuntimeOptions = {
 				summaryOptions: {},
 				gcOptions: {},
 				loadSequenceNumberVerification: "close",
@@ -1501,8 +1501,9 @@ describe("Runtime", () => {
 				enableRuntimeIdCompressor: undefined,
 				enableGroupedBatching: true, // Redundant, but makes the JSON.stringify yield the same result as the logs
 				explicitSchemaControl: false,
-			};
-			const mergedRuntimeOptions = { ...defaultRuntimeOptions, ...runtimeOptions };
+				enablePathBasedAddressing: false,
+			} as const satisfies IContainerRuntimeOptionsInternal;
+			const mergedRuntimeOptions = { ...defaultRuntimeOptions, ...runtimeOptions } as const;
 
 			it("Container load stats", async () => {
 				await ContainerRuntime.loadRuntime({
